b5da154655502d1217cb1196a70c10f439566265
Martijn Blankestijn <martijnblankestijn@gmail.com>
2020-08-28 13:15:13 +0200

    Small adjustements
    Small adjustements

--- src/test/java/postgres/ChangedBaseTypeTest.java
+++ src/test/java/postgres/ChangedBaseTypeTest.java
@@ -13,8 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ChangedBaseTypeTest {
   @Test
   public void showChange() throws Exception {
-    String url = "jdbc:postgresql://localhost:5432/it_ais_longterm?user=integrationtest&password=integrationtest";
-    url = "jdbc:postgresql://localhost:5432/?user=postgres&password=postgres&loggerLevel=TRACE&loggerFile=pgjdbc-trace.log ";
+    String url = "jdbc:postgresql://localhost:5432/?user=postgres&password=postgres&loggerLevel=TRACE&loggerFile=pgjdbc-trace.log ";
     Connection connection = DriverManager.getConnection(url);
     try {
       connection.createStatement().executeUpdate("DROP TABLE IF EXISTS X");
@@ -25,10 +24,15 @@ public class ChangedBaseTypeTest {
 
       ResultSet result = connection.createStatement().executeQuery("select * from X");
       ResultSetMetaData metaData = result.getMetaData();
-      System.out.println("Index: $i, column name: [${metadata.getColumnName(i)}] type: [ " +
-          result.getMetaData().getColumnType(2) + ", typeName: [" + metaData.getColumnTypeName(2));
+      int columnIndex = 2;
+
+      System.out.println("column name: [" + metaData.getColumnName(columnIndex) +
+          "] type: [ " + result.getMetaData().getColumnType(columnIndex) +
+          "], typeName: [" + metaData.getColumnTypeName(columnIndex) + "]");
       result.next();
-      Array array = result.getArray(2);
+
+      Array array = result.getArray(columnIndex);
+
       System.out.println("Array: base type name: " + array.getBaseTypeName() + ", base type: " + array.getBaseType());
       Object[] arr = (Object[]) array.getArray();
       for (Object a : arr) {
